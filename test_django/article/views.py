33adeb3ed20a7a0885dddf5e8600efda21d114d2
Dmitriy.Klimkov <dmitriy.klimkov@rt.ru>
2020-06-25 18:33:13 +0200

    добавлена возможность обновлять данные статей через API

--- test_django/article/views.py
+++ test_django/article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Article
@@ -22,3 +23,13 @@ class ArticleView(APIView):
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({
+            "success": "Article '{}' updated successfully".format(article_saved.title)
+        })
